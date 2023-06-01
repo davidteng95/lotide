c886feb8ee478d5a86ecb027bdff324969c834f9
David <davidteng95@gmail.com>
2023-06-01 06:26:06 +0000

    Implement without assignment

---
+++ without.js
@@ -0,0 +1,45 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  for (let i = 0; i <array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+const without = function(array, subset) {
+  const result = [];
+  for (let i of array) {
+    let found = false;
+    for (let j of subset) {
+      if (i === j) {
+        found = true;
+        break;
+      }
+    }
+
+    if (found === false) {
+      result.push(i);
+    }
+  }
+  return result;
+};
+
+console.log(without([1, 2, 3], [1]));
+console.log(without(["1", "2", "3"], [1, 2, "3"]));
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]);
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
