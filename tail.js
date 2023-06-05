f7cdad1c5603e9c32c051537d0d3fa54ce6095bc
David <davidteng95@gmail.com>
2023-06-05 22:46:51 +0000

    refractor file, using module.exports and require to call existing functions

--- tail.js
+++ tail.js
@@ -1,19 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = function(array) {
   return array.slice(1);
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-const empty = [];
-tail(empty);
-assertEqual(empty.length, 0);
+module.exports = tail;
