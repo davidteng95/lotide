756bbb961a24e5508fc2ecbf26df95f75481de92
David <davidteng95@gmail.com>
2023-06-06 02:06:22 +0000

    Incorporate assertEqual tests to Mocha and Chai tests

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,16 +1,9 @@
-//const assertEqual = require('../assertEqual');
 const assert = require('chai').assert;
 const tail = require('../tail');
 
-//Test code
 const words = ["Yo Yo", "Lighthouse", "Labs"];
-// tail(words); // no need to capture the return value since we are not checking it
-// console.log(tail(words)); // no need to capture the return value since we are not checking it
-// assertEqual(words.length, 3); // original array should still have 3 elements!
 
 const empty = [];
-// tail(empty);
-// assertEqual(empty.length, 0);
 
 describe("#tail", () => {
   it("returns ['Lighthouse', 'Labs'] for const words = ['Yo Yo', 'Lighthouse', 'Labs']", () => {
