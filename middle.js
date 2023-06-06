d100f355fedc856c80a177685df9c96a2b30307c
David <davidteng95@gmail.com>
2023-06-06 00:10:40 +0000

    refractor file, using module.exports and require to call existing functions

--- middle.js
+++ middle.js
@@ -1,24 +1,3 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
-
 const middle = function(array) {
   let length = array.length;
   if (length <= 2) {
@@ -35,12 +14,4 @@ const middle = function(array) {
   }
 };
 
-
-console.log(middle([1])); // => []
-console.log(middle([1, 2]));// => []
-
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
+module.exports = middle;
